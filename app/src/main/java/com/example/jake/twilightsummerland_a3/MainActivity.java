d599fb8fe3585fb71f5fdec1893b77b9f16a3499
T <twisummerland@gmail.com>
2017-11-17 00:52:29 -0800

    initial commit assignment 3

---
+++ app/src/main/java/com/example/jake/twilightsummerland_a3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jake.twilightsummerland_a3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
